c3fdef7d477f7ace4d6cc2420c7900a02023ee4f
Katerina Koller <koller.katerina@gmail.com>
2025-06-29 07:07:47 +0200

    active cell focus

--- editor-tabulek.js
+++ editor-tabulek.js
@@ -3,41 +3,48 @@ let activeCell;
 
 //create table and buttons
 window.onload = () => {
-    createDefaultButtons();
-    createDefaultTable(3, 5);    
-}
-
+  createDefaultButtons();
+  createDefaultTable(3, 5);
+};
 
 /**
  * function for creating table
  * @returns new table
  */
 function createDefaultTable(rows, columns) {
-    table = document.createElement("table");
-    document.body.appendChild(table);
+  table = document.createElement("table");
+  document.body.appendChild(table);
 
-    //create rows
-    for (let y = 0; y < rows; y++) {
-        let tr = document.createElement("tr");
-        table.appendChild(tr);
+  //create rows
+  for (let y = 0; y < rows; y++) {
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
 
-        //create columns
-        for (let x = 0; x < columns; x++) {
-            tr.appendChild(createCell());
-        }
+    //create columns
+    for (let x = 0; x < columns; x++) {
+      tr.appendChild(createCell());
     }
+  }
+
+  const firstActiveCell = document.getElementsByTagName("input");
+  firstActiveCell[0].focus();
 }
 
 /**
  * function for creating main buttons
  */
 function createDefaultButtons() {
-    createAndInsertButton("Přidat řádek dolů", document.body).onclick = addRowBelow;
-    createAndInsertButton("Přidat řádek nahoru", document.body).onclick = addRowAbove;
-    createAndInsertButton("Přidat sloupec vlevo", document.body).onclick = addColumnLeft;
-    createAndInsertButton("Přidat sloupec vpravo", document.body).onclick = addColumnRight;
-    createAndInsertButton("Odstranit řádek", document.body).onclick = deleteRow;
-    createAndInsertButton("Odstranit sloupec", document.body).onclick = deleteColumn;
+  createAndInsertButton("Přidat řádek dolů", document.body).onclick =
+    addRowBelow;
+  createAndInsertButton("Přidat řádek nahoru", document.body).onclick =
+    addRowAbove;
+  createAndInsertButton("Přidat sloupec vlevo", document.body).onclick =
+    addColumnLeft;
+  createAndInsertButton("Přidat sloupec vpravo", document.body).onclick =
+    addColumnRight;
+  createAndInsertButton("Odstranit řádek", document.body).onclick = deleteRow;
+  createAndInsertButton("Odstranit sloupec", document.body).onclick =
+    deleteColumn;
 }
 
 /**
@@ -45,33 +52,33 @@ function createDefaultButtons() {
  * @returns new cell
  */
 function createCell() {
-    let td = document.createElement("td");
-    let tdInput = document.createElement("input");
-    tdInput.type = "text";
+  let td = document.createElement("td");
+  let tdInput = document.createElement("input");
+  tdInput.type = "text";
 
-    //v this bude input, ktery vyvola udalost
-    tdInput.onfocus = function () {
-        activeCell = this;
-    }
+  //v this bude input, ktery vyvola udalost
+  tdInput.onfocus = function () {
+    activeCell = this;
+  };
 
-    td.appendChild(tdInput);
+  td.appendChild(tdInput);
 
-    return td;
+  return td;
 }
 
 /**
  * function for creating button
- * @param {button title} description 
- * @param {button's parent} parent 
+ * @param {button title} description
+ * @param {button's parent} parent
  * @returns new button
  */
 function createAndInsertButton(description, parent) {
-    //let btnDiv = document.createElement("div");
-    let btn = document.createElement("button");
-    btn.textContent = description;
-    //btnDiv.appendChild(btn);
-    parent.appendChild(btn);
-    return btn;
+  //let btnDiv = document.createElement("div");
+  let btn = document.createElement("button");
+  btn.textContent = description;
+  //btnDiv.appendChild(btn);
+  parent.appendChild(btn);
+  return btn;
 }
 
 /**
@@ -79,17 +86,18 @@ function createAndInsertButton(description, parent) {
  * @returns new row
  */
 function createRow() {
-    /*
-    * table = <TABLE>
-    * table.firstElementChild = <TR>
-    * table.firstElementChild.childNodes = [<TD>]
-    * table.firstElementChild.childNodes.length = number (of tds)
-    */
-    let newRow = document.createElement("tr");
-    for (let i = 0; i< table.firstElementChild.childNodes.length; i++) {
-        newRow.appendChild(createCell());
-    }
-    return newRow;
+  /*
+   * table = <TABLE>
+   * table.firstElementChild = <TR>
+   * table.firstElementChild.childNodes = [<TD>]
+   * table.firstElementChild.childNodes.length = number (of tds)
+   */
+  let newRow = document.createElement("tr");
+  for (let i = 0; i < table.firstElementChild.childNodes.length; i++) {
+    newRow.appendChild(createCell());
+  }
+
+  return newRow;
 }
 
 /**
@@ -97,82 +105,98 @@ function createRow() {
  * @returns index of active row
  */
 function findActiveCellRowIndex() {
-    let searchedArea = table.childNodes; //table.childNodes returns array of table rows
-    let searchedElement = activeCell.parentElement.parentElement //active cell is input, first parent is td, second parent is tr
-    return Array.prototype.indexOf.call(searchedArea, searchedElement);
+  let searchedArea = table.childNodes; //table.childNodes returns array of table rows
+  let searchedElement = activeCell.parentElement.parentElement; //active cell is input, first parent is td, second parent is tr
+  return Array.prototype.indexOf.call(searchedArea, searchedElement);
 }
 
 /**
  * function for adding a row above the active cell
  */
 function addRowAbove() {
-    let row = createRow();
-    let activeIndex = findActiveCellRowIndex();
-    table.insertBefore(row, table.childNodes[activeIndex]);
+  activeCell.focus();
+
+  let row = createRow();
+  let activeIndex = findActiveCellRowIndex();
+  table.insertBefore(row, table.childNodes[activeIndex]);
 }
 
 /**
  * function for adding row below the active cell
  */
 function addRowBelow() {
-    let row = createRow();
-    let activeIndex = findActiveCellRowIndex();
-    if (table.lastChild == table.childNodes[activeIndex]) {
-        table.appendChild(row);
-    }
-    else {
-        table.insertBefore(row, table.childNodes[activeIndex+1]);
-    }
+  activeCell.focus();
+  let row = createRow();
+  let activeIndex = findActiveCellRowIndex();
+  if (table.lastChild == table.childNodes[activeIndex]) {
+    table.appendChild(row);
+  } else {
+    table.insertBefore(row, table.childNodes[activeIndex + 1]);
+  }
 }
 
 /**
  * function for finding index of active cell's column
  */
 function findActiveCellColumnIndex() {
-    let cellsInRow = activeCell.parentElement.parentElement.childNodes; //active cell is input, first parent is td, second parent is tr, tr's childs are all cells in the row
-    let td = activeCell.parentElement;
-    return Array.prototype.indexOf.call(cellsInRow, td);
+  let cellsInRow = activeCell.parentElement.parentElement.childNodes; //active cell is input, first parent is td, second parent is tr, tr's childs are all cells in the row
+  let td = activeCell.parentElement;
+  return Array.prototype.indexOf.call(cellsInRow, td);
 }
 
 /**
  * function for adding a column to the left from the active cell
  */
 function addColumnLeft() {
-    let activeIndex = findActiveCellColumnIndex();
-    for (let i = 0; i< table.childNodes.length; i++) {
-        table.childNodes[i].insertBefore(createCell(), table.childNodes[i].childNodes[activeIndex])
-    }
+  activeCell.focus();
+
+  let activeIndex = findActiveCellColumnIndex();
+  for (let i = 0; i < table.childNodes.length; i++) {
+    table.childNodes[i].insertBefore(
+      createCell(),
+      table.childNodes[i].childNodes[activeIndex]
+    );
+  }
 }
 
 /**
  * function for adding a column to the right from the active cell
  */
 function addColumnRight() {
-    let activeIndex = findActiveCellColumnIndex();
-    for (let i = 0; i< table.childNodes.length; i++) {
-        if(table.childNodes[i].childNodes[activeIndex]==table.childNodes[i].lastElementChild) {
-            table.childNodes[i].appendChild(createCell());
-        }
-        else {
-            table.childNodes[i].insertBefore(createCell(), table.childNodes[i].childNodes[activeIndex+1])
-        }
+  activeCell.focus();
+
+  let activeIndex = findActiveCellColumnIndex();
+  for (let i = 0; i < table.childNodes.length; i++) {
+    if (
+      table.childNodes[i].childNodes[activeIndex] ==
+      table.childNodes[i].lastElementChild
+    ) {
+      table.childNodes[i].appendChild(createCell());
+    } else {
+      table.childNodes[i].insertBefore(
+        createCell(),
+        table.childNodes[i].childNodes[activeIndex + 1]
+      );
     }
+  }
 }
 
 /**
  * function for deleting a row of an active cell
  */
 function deleteRow() {
-    let activeIndex = findActiveCellColumnIndex();
-    table.removeChild(table.childNodes[activeIndex]);
+  let activeIndex = findActiveCellColumnIndex();
+  table.removeChild(table.childNodes[activeIndex]);
 }
 
 /**
  * function for deleting a column of an active cell
  */
 function deleteColumn() {
-    let activeIndex = findActiveCellColumnIndex();
-    for (let i = 0; i<table.childNodes.length; i++) {
-        table.childNodes[i].removeChild(table.childNodes[i].childNodes[activeIndex]);
-    }
+  let activeIndex = findActiveCellColumnIndex();
+  for (let i = 0; i < table.childNodes.length; i++) {
+    table.childNodes[i].removeChild(
+      table.childNodes[i].childNodes[activeIndex]
+    );
+  }
 }
